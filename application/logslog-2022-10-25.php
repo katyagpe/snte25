ba443a3f80b6e42103992d0dfa85c71c27498e3a
Katya Guadalupe Salas Rodríguez <salas-rodriguez@hotmail.com>
2022-10-25 14:18:04 -0500

    framework para el sitio

---
+++ application/logslog-2022-10-25.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2022-10-25 21:15:40 --> Config Class Initialized
+INFO - 2022-10-25 21:15:40 --> Hooks Class Initialized
+DEBUG - 2022-10-25 21:15:40 --> UTF-8 Support Enabled
+INFO - 2022-10-25 21:15:40 --> Utf8 Class Initialized
+INFO - 2022-10-25 21:15:40 --> URI Class Initialized
+DEBUG - 2022-10-25 21:15:40 --> No URI present. Default controller set.
+INFO - 2022-10-25 21:15:40 --> Router Class Initialized
+INFO - 2022-10-25 21:15:41 --> Output Class Initialized
+INFO - 2022-10-25 21:15:41 --> Security Class Initialized
+DEBUG - 2022-10-25 21:15:41 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-10-25 21:15:41 --> Input Class Initialized
+INFO - 2022-10-25 21:15:41 --> Language Class Initialized
+INFO - 2022-10-25 21:15:41 --> Loader Class Initialized
+INFO - 2022-10-25 21:15:41 --> Helper loaded: url_helper
+INFO - 2022-10-25 21:15:41 --> Helper loaded: form_helper
+INFO - 2022-10-25 21:15:41 --> Form Validation Class Initialized
+INFO - 2022-10-25 21:15:41 --> Controller Class Initialized
+INFO - 2022-10-25 21:15:41 --> File loaded: C:\xampp\htdocs\snte25\application\views\welcome_message.php
+INFO - 2022-10-25 21:15:41 --> Final output sent to browser
+DEBUG - 2022-10-25 21:15:41 --> Total execution time: 0.1285
