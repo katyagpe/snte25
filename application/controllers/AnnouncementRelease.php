fdfe989665f833e7dc602ba200fc14250da4e2f9
Katya Guadalupe Salas Rodríguez <salas-rodriguez@hotmail.com>
2022-11-03 16:45:13 -0600

    update code

--- application/controllers/AnnouncementRelease.php
+++ application/controllers/AnnouncementRelease.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class AnnouncementRelease extends CI_Controller
+class Announcementrelease extends CI_Controller
 {
     public function __construct()
     {
